e3b18a9eec81286010d179828be0c4cfaca060ee
jcleilton <jcleilton@gmail.com>
2018-07-18 06:31:58 -0300

    Primeiro Commit 18-07-18

--- src/app/objetivos/objetivos.component.ts
+++ src/app/objetivos/objetivos.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjetivosComponent implements OnInit {
 
+  interesting = 'Analista de sistemas desenvolvedor desktop, web e mobile';
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
